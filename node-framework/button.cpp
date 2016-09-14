184b6a4a952d780335280b057f90469d242c2b55
Daniel Schulte <daniel@schultes-home.de>
2016-09-14 21:01:07 +0200

    button: Use interrupts

--- node-framework/button.cpp
+++ node-framework/button.cpp
@@ -1,24 +1,12 @@
 #include "button.h"
 #include <Arduino.h>
 
-button::button()
+void button::isr()
 {
-
-}
-
-void button::begin(unsigned char pin, gpio_pin::pin_dir dir)
-{
-    _pin.begin(pin, dir);
-    this->pin=pin;
-    /*isr=allocate_trampoline([this]
-    {
-
-    });
-    attachInterrupt(digitalPinToInterrupt(pin), tramps[isr], CHANGE);*/
-}
-
-void button::update()
-{
+    static bool in_isr=false;
+    if(in_isr)
+        return;
+    in_isr=true;
     bool button=digitalRead(pin);
     unsigned long time_now=millis();
 
@@ -28,8 +16,8 @@ void button::update()
         && (time_now-last_click_stop)>timeout_double_click) // Enough time has passed since the last click
     {
         first_click_detected=false;
-        if(cb_short)
-            cb_short();
+        //Serial.println("Short");
+        trigger=event_trigger::single_short;
     }
 
     // Button state changed
@@ -38,6 +26,7 @@ void button::update()
         // Button just became pressed
         if(last_button_state && !button)
         {
+            //Serial.println("Button pressed");
             hold_start=millis();
             button_down=true;
         }
@@ -45,41 +34,103 @@ void button::update()
         else if(!last_button_state && button)
         {
             unsigned long hold_time=time_now-hold_start;
+            //Serial.print("Held for ");
+            //Serial.println(hold_time);
             button_down=false;
             // Short Click
-            if(hold_time>=short_click && hold_time<=medium_click)
+            if(hold_time>=click_minimum && hold_time<=medium_click)
             {
-                // Remember first click
-                if(!first_click_detected)
+                if(cb_double_short)
                 {
-                    first_click_detected=true;
-                    last_click_stop=time_now;
+                    // Remember first click
+                    if(!first_click_detected)
+                    {
+                        //Serial.println("First click detected");
+                        first_click_detected=true;
+                        last_click_stop=time_now;
+                    }
+                    // Check if double click
+                    else
+                    {
+                        //Serial.print("Potential double ");
+                        unsigned long time_between_clicks=hold_start-last_click_stop;
+                        //Serial.println(time_between_clicks);
+                        first_click_detected=false;
+                        if(time_between_clicks<=timeout_double_click)
+                        {
+                            //Serial.println("Short double");
+                            trigger=event_trigger::double_short;
+                        }
+                    }
                 }
-                // Check if double click
                 else
                 {
-                    unsigned long time_between_clicks=hold_start-last_click_stop;
                     first_click_detected=false;
-                    if(time_between_clicks<=timeout_double_click && cb_double_short)
-                    {
-                        cb_double_short();
-                    }
+                    trigger=event_trigger::single_short;
                 }
             }
             // Medium Click
             else if(hold_time>=medium_click && hold_time<=long_click)
             {
                 first_click_detected=false;
-                if(cb_medium) {cb_medium();}
+                //Serial.println("Medium");
+                if(cb_medium)
+                    trigger=event_trigger::single_medium;
+                else if(cb_short)
+                    trigger=event_trigger::single_short;
             }
+            // Long Click
             else if(hold_time>=long_click)
             {
                 first_click_detected=false;
-                if(cb_long) {cb_long();}
+                if(cb_long)
+                {
+                //Serial.println("Long");
+                trigger=event_trigger::single_long;
+                }
+                else if(cb_medium)
+                {
+                    trigger=event_trigger::single_medium;
+                }
+                else if(cb_short)
+                {
+                    trigger=event_trigger::single_short;
+                }
             }
         }
         last_button_state=button;
     }
+    in_isr=false;
+}
+
+button::button()
+{
+
+}
+
+void button::begin(unsigned char pin, gpio_pin::pin_dir dir)
+{
+    _pin.begin(pin, dir);
+    this->pin=pin;
+    last_button_state=_pin.read();
+
+    isr_index=allocate_trampoline(std::bind(&button::isr, this));
+    attachInterrupt(digitalPinToInterrupt(pin), tramps[isr_index], CHANGE);
+}
+
+void button::update()
+{
+    isr();
+    if(trigger==event_trigger::single_short && cb_short)
+        cb_short();
+    else if(trigger==event_trigger::single_medium && cb_medium)
+        cb_medium();
+    else if(trigger==event_trigger::single_long && cb_long)
+        cb_long();
+    else if(trigger==event_trigger::double_short && cb_double_short)
+        cb_double_short();
+
+    trigger=event_trigger::none;
 }
 
 void button::on_short_click(button::callback cb)
--- node-framework/button.h
+++ node-framework/button.h
@@ -11,20 +11,34 @@ class button: public ticker_component
 public:
     typedef std::function<void(void)> callback;
 private:
+    enum class event_trigger: unsigned char
+    {
+        none,
+        single_short,
+        single_medium,
+        single_long,
+        double_short,
+    };
+
     gpio_pin _pin;
     unsigned char pin;
     int isr_index=-1;
     callback cb_short, cb_medium, cb_long, cb_double_short;
+
     unsigned long hold_start=0;
-    unsigned long last_click_stop=0;
+    unsigned long last_click_stop=0;    
     bool first_click_detected=false;
     bool button_down=false;
     bool last_button_state=false;
 
-    const unsigned long timeout_double_click=200;
-    const unsigned long short_click=150;
-    const unsigned long medium_click=300;
-    const unsigned long long_click=450;
+    const unsigned long timeout_double_click=500;
+    const unsigned long click_minimum=25;
+    //const unsigned long short_click=150;
+    const unsigned long medium_click=250;
+    const unsigned long long_click=400;
+
+    event_trigger trigger;
+    void isr();
 public:
     button();
     virtual ~button()=default;
