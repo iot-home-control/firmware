a869281f0e443e4d134f1218ea81fa5901a204bf
Daniel Schulte <daniel@schultes-home.de>
2017-11-12 22:46:17 +0100

    node_temperature_sensor: Use config_loader

--- node-framework/node_temperature_sensor.cpp
+++ node-framework/node_temperature_sensor.cpp
@@ -5,21 +5,61 @@
 
 #include "sensor_ds1820.h"
 #include "sensor_dht22.h"
+#include "config_loader.h"
+#include <ArduinoJson.h>
 
 class node_temperature_sensor: public node_base
 {
 public:
     void setup() override;
     void loop() override;
+    void on_wifi_connected() override;
 private:
-    sensor_dht22 dht22;
-    sensor_ds1820 ds1820;
+    //sensor_dht22 dht22;
+    //sensor_ds1820 ds1820;
+    config_loader loader;
 };
 
 void node_temperature_sensor::setup()
 {
     node_base::setup();
-    dht22.begin(5,10000);
+
+    pinMode(0, INPUT_PULLUP);
+
+    loader.install_factory("dht22", 0, [this](const JsonObject &obj, int type_count) -> ticker_component*
+    {
+        if(!obj.containsKey("pin") || !obj["pin"].is<int>())
+        {
+            Serial.println("Can't create dht22. Pin is not set or not an int");
+            return nullptr;
+        }
+        int refresh_rate = 10;
+        if(obj.containsKey("rate") && obj["rate"].is<int>())
+            refresh_rate = obj["rate"].as<int>();
+        int pin = obj["pin"].as<int>();
+        sensor_dht22 *sensor = new sensor_dht22();
+        sensor->begin(pin, refresh_rate*1000);
+
+        sensor->on_temperature_changed=[this,type_count](const float v)
+        {
+            Serial.print("Temperature changed ");
+            Serial.println(v);
+            mqtt.publish(get_state_topic("temperature",type_count),"local,"+String(v));
+        };
+
+        sensor->on_humidity_changed=[this,type_count](const float v)
+        {
+            Serial.print("Humidity changed ");
+            Serial.println(v);
+            mqtt.publish(get_state_topic("humidity",type_count),"local,"+String(v));
+        };
+
+        return sensor;
+    });
+
+    loader.begin(components, digitalRead(0)==LOW);
+
+    /*dht22.begin(5,10000);
     dht22.on_temperature_changed=[this](const float v)
     {
         Serial.print("Temperature changed ");
@@ -43,13 +83,24 @@ void node_temperature_sensor::setup()
         Serial.println(v);
         mqtt.publish(get_state_topic("temperature",vnode_offest+index),"local,"+String(v));
     };
-    components.push_back(&ds1820);
+    components.push_back(&ds1820);*/
 }
 
 void node_temperature_sensor::loop()
 {
     node_base::loop();
     node_base::wait_for_loop_timing();
+}
+
+void node_temperature_sensor::on_wifi_connected()
+{
+    node_base::on_wifi_connected();
+
+    if(loader.check(device_id, CONFIG_SERVER))
+    {
+        Serial.println("/config.json was updated. Reboot...");
+        ESP.restart();
+    }
 }
 
 node_temperature_sensor node;
