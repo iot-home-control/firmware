48b04659574725b4fc61d255d227644f0ee75fce
Daniel Schulte <trilader@schroedingers-bit.net>
2019-01-06 18:23:41 +0100

    Fix directory nesting level issue

--- stacktrace.py
+++ stacktrace.py
@@ -6,7 +6,7 @@ import re
 import sys
 
 if __name__=="__main__":
-    addr2line="../esp8266-sdk/tools/xtensa-lx106-elf/bin/xtensa-lx106-elf-addr2line"
+    addr2line="../../esp8266-sdk/tools/xtensa-lx106-elf/bin/xtensa-lx106-elf-addr2line"
     elf="./node-framework/build/{}.elf".format(sys.argv[1])
     
     regex = re.compile("(40[0-2][0-9a-f]{5}\\b)")
