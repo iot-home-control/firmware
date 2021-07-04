00e70381086d0832fa825afaa2d69107e3589439
Hinrikus Wolf <mail@hinrikus-wolf.de>
2021-07-04 17:40:39 +0200

    update stacktrace.py for pio

--- stacktrace.py
+++ stacktrace.py
@@ -7,7 +7,7 @@ import sys
 
 if __name__=="__main__":
     addr2line="../../esp8266-sdk/tools/xtensa-lx106-elf/bin/xtensa-lx106-elf-addr2line"
-    elf="./build/{}.elf".format(sys.argv[1])
+    elf=".pio/build/{}/firmware.elf".format(sys.argv[1])
     
     regex = re.compile("(40[0-2][0-9a-f]{5}\\b)")
     
