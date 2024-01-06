b0349b37c3709df27156ec8521b75f9e312cf2dd
Hinrikus Wolf <mail@hinrikus-wolf.de>
2024-01-06 15:32:08 +0100

    buildinfo: Try to get git revision using `git describe` if possible
    Mark as dirty using *. Use ? as marker when git binary isn't available to indicate unknown dirty status.

--- make_buildinfo.py
+++ make_buildinfo.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python3
 
 import datetime
+import subprocess
 from pathlib import Path
+import shutil
 
 cwd = Path(File('sources.lib').srcnode().abspath).parent
 git_dir = cwd / ".git"
 
 if git_dir.exists():
-    with open(git_dir / "HEAD", "r") as f:
-        head_ref = f.read().strip()
-    refname = head_ref.split(":")[1].strip()
-    with open(git_dir / refname, "r") as f:
-        git_version = f.read().strip()[:7]
+    if shutil.which("git") is None:
+        with open(git_dir / "HEAD", "r") as f:
+            head_ref = f.read().strip()
+        refname = head_ref.split(":")[1].strip()
+        with open(git_dir / refname, "r") as f:
+            git_version = f.read().strip()[:7] + "?"
+    else:
+        git_version = subprocess.check_output(["git", "describe", "--always", "--dirty=*"]).strip()
 else:
     git_version = "unknown"
 
